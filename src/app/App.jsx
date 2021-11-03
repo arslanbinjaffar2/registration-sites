c1b5bda8cab7972fe443c6188d07002581f43356
m_waris <mwa@eventbuizz.com>
2021-11-03 18:37:35 +0500

    master

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import 'sass/app.scss';
 import RouterOutlet from 'router/RouterOutlet'
+import Header from "./components/layout/Header";
 class App extends Component {
   render() {
     return (
       <div id="App">
+        <Header />
         <RouterOutlet/>
       </div>
     );
