8e052edb714c5343855de9bb5a63a18801dd6509
Muhammad Ahmad <mah@eventbuizz.com>
2022-01-04 12:12:24 +0500

    Updated

--- src/router/RouterOutlet.jsx
+++ src/router/RouterOutlet.jsx
@@ -1,27 +1,27 @@
 import * as React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MasterLayoutRoute from "@/layout/MasterLayoutRoute";
-import Header from "@/themes/theme-1/header/Header";
 import Index from "@/Index";
 import FullPage from "@/FullPage";
 import Home from "@/Home";
+import Header from "@/modules/Header";
 import Error404 from "@/Error404";
-import AttendeesPage from "../app/components/pages/AttendeesPage";
-import GalleryPage from "../app/components/pages/GalleryPage";
-import ExhibitorPage from "../app/components/pages/ExhibitorPage";
-import PhotosPage from "../app/components/pages/PhotosPage";
-import ProgramPage from "../app/components/pages/ProgramPage";
-import SpeakerPage from "../app/components/pages/SpeakerPage";
-import SponsorPage from "../app/components/pages/SponsorPage";
-import StreamingPage from "../app/components/pages/StreamingPage";
-import TimetablePage from "../app/components/pages/TimetablePage";
-import VideoPage from "../app/components/pages/VideoPage";
-import CmsPage from "../app/components/pages/CmsPage";
-import CustomPage from "../app/components/pages/CustomPage";
-import NewsPage from "../app/components/pages/news/NewsPage";
-import NewsDetailPage from "../app/components/pages/news/NewsDetailPage";
+import AttendeesPage from "@/pages/AttendeesPage";
+import GalleryPage from "@/pages/GalleryPage";
+import ExhibitorPage from "@/pages/ExhibitorPage";
+import PhotosPage from "@/pages/PhotosPage";
+import ProgramPage from "@/pages/ProgramPage";
+import SpeakerPage from "@/pages/SpeakerPage";
+import SponsorPage from "@/pages/SponsorPage";
+import StreamingPage from "@/pages/StreamingPage";
+import TimetablePage from "@/pages/TimetablePage";
+import VideoPage from "@/pages/VideoPage";
+import CmsPage from "@/pages/CmsPage";
+import CustomPage from "@/pages/CustomPage";
+import NewsPage from "@/pages/news/NewsPage";
+import NewsDetailPage from "@/pages/news/NewsDetailPage";
 import MasterLayoutMyAccount from "@/layout/MasterLayoutMyAccount";
-import AttendeeLayout from '@/AttendeeLayout';
+import AttendeeLayout from "@/AttendeeLayout";
 import MyProfile from "@/themes/theme-1/profile/MyProfile";
 import MyProfileEdit from "@/themes/theme-1/profile/MyProfileEdit";
 
@@ -121,9 +121,21 @@ class RouterOutlet extends React.Component {
               exact
               path="/:event_url/custom/:id"
             />
-            <MasterLayoutMyAccount component={MyProfile} exact path="/:event_url/profile" />
-            <MasterLayoutMyAccount component={MyProfileEdit} exact path="/:event_url/profile/edit" />
-            <Route component={AttendeeLayout} exact path="/:event_url/attendees" />
+            <MasterLayoutMyAccount
+              component={MyProfile}
+              exact
+              path="/:event_url/profile"
+            />
+            <MasterLayoutMyAccount
+              component={MyProfileEdit}
+              exact
+              path="/:event_url/profile/edit"
+            />
+            <Route
+              component={AttendeeLayout}
+              exact
+              path="/:event_url/attendees"
+            />
             <Route component={Error404} />
           </Switch>
         </React.Fragment>
