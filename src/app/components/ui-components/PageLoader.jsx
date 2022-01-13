09188166517c6808fe9eeb5647aa396ec6991870
Hashmat Abbas <hab@eventbuizz.com>
2022-01-13 19:16:41 +0500

    Master

--- src/app/components/ui-components/PageLoader.jsx
+++ src/app/components/ui-components/PageLoader.jsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 
 const PageLoader = ({ className, fixed, title, description }) => {
     return (
-        <div id="loader-wrapper" className={`${className && className} ${fixed && 'popup-fixed'}`}>
+        <div id="loader-wrapper" className={`${className ? className : ''} ${fixed && 'popup-fixed'}`}>
             {className ? (
                 <div className="wrapper_laoder">
-                    <h2>{title}</h2>
-                    <p>{description} </p>
+                    {title && <h2>{title}</h2>}
+                    {description && <p>{description} </p>}
                     <div id="loader"></div>
                 </div>
             ) : (
