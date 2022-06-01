b0e5b869133b6f62cb939ba73cd3bfd727d82ea0
ahmad13054 <mah@eventbuizz.com>
2022-06-01 10:30:26 +0500

    Updated

--- src/app/components/themes/theme-1/attendee/Variation5.jsx
+++ src/app/components/themes/theme-1/attendee/Variation5.jsx
@@ -1,9 +1,162 @@
-import React from 'react'
-
-const Variation5 = () => {
+import React from "react";
+import { Link } from "react-router-dom";
+const Variation5 = ({ attendees, searchBar, loadMore, event }) => {
   return (
-    <div>Variation5</div>
-  )
-}
+    <div
+      style={{
+        padding: "50px 0",
+      }}
+      className="edgtf-parallax-section-holder"
+    >
+      <div className="container">
+        <div className="row d-flex mb-5">
+          <div className="col-12">
+            <div className="edgtf-title-section-holder text-center">
+              <h2
+                className="edgtf-title-with-dots edgtf-appeared"
+              >
+                {event.labels.EVENTSITE_ATTENDEES}
+              </h2>
+              <span className="edge-title-separator edge-enable-separator"></span>
+            </div>
+          </div>
+        </div>
+        {searchBar()}
+        <div className="row d-flex edgtf-team-list-holder edgtf-team-info-below-image">
+          {/* Grid */}
+          {attendees &&
+            attendees.map((attendee, i) => (
+              <div
+                key={i}
+                className="col-12 col-sm-6 col-md-3 pl-0 pr-0 ebs-attendee-v1"
+              >
+                <div className="edgtf-team-list-holder-inner info_box">
+                  <div className="edgtf-team edgtf-team-light w-100 mb-5">
+                    <div className="edgtf-team-inner">
+                      <div className="edgtf-team-image">
+                        <Link to={`/${event.url}/attendees/${attendee.id}`}>
+                          <img
+                            style={{ width: "100%" }}
+                            src={
+                              attendee.image && attendee.image !== ""
+                                ? process.env.REACT_APP_EVENTCENTER_URL +
+                                  "/assets/attendees/" +
+                                  attendee.image
+                                : require("img/square.jpg")
+                            }
+                            alt="g"
+                          />
+                        </Link>
+                      </div>
+                      {/* Description */}
+                      <div className="edgtf-team-info">
+                        <div className="edgtf-team-title-holder">
+                          {(attendee.first_name || attendee.last_name) && (
+                            <Link to={`/${event.url}/attendees/${attendee.id}`}>
+                              <h3 className="edgtf-team-name">
+                                {attendee.first_name && attendee.first_name}{" "}
+                                {attendee.last_name && attendee.last_name}
+                              </h3>
+                            </Link>
+                          )}
+                          {attendee.info &&
+                            (attendee.info.company_name ||
+                              attendee.info.title) && (
+                              <div className="ebs-attendee-designation">
+                                {attendee.info.title && attendee.info.title}
+                                {attendee.info.company_name &&
+                                  attendee.info.title &&
+                                  " "}
+                                {attendee.info.company_name &&
+                                  attendee.info.company_name}
+                              </div>
+                            )}
+                          {attendee.email && (
+                            <div style={{color: '#666666'}} className="ebs-email-phone">
+                              <a
+                                href={`mailto:${attendee.email}`}
+                                className="edgtf-team-position"
+                                style={{color: '#666666'}}
+                              >
+                                {attendee.email}
+                              </a>
+                            </div>
+                          )}
 
-export default Variation5
+                          {attendee.phone && (
+                            <div style={{color: '#666666'}} className="ebs-email-phone">
+                              <a
+                                href={`tel: ${attendee.phone}`}
+                                className="edgtf-team-position"
+                                style={{color: '#666666'}}
+                              >
+                                {attendee.phone}
+                              </a>
+                            </div>
+                          )}
+                        </div>
+                        {attendee.info &&
+                          (attendee.info.facebook ||
+                            attendee.info.twitter ||
+                            attendee.info.linkedin ||
+                            attendee.info.website) && (
+                            <div className="edgtf-team-social-holder-between">
+                              <div className="edgtf-team-social">
+                                <div className="edgtf-team-social-inner">
+                                  <div className="edgtf-team-social-wrapp">
+                                    <div className="social-icons">
+                                      {attendee.info.facebook && (
+                                        <a
+                                          target="_blank"
+                                          href={`${attendee.info.facebook_protocol}${attendee.info.facebook}`}
+                                        >
+                                          <span data-icon="&#xe0aa;"></span>
+                                        </a>
+                                      )}
+                                      {attendee.info.twitter && (
+                                        <a
+                                          target="_blank"
+                                          href={`${attendee.info.twitter_protocol}${attendee.info.twitter}`}
+                                        >
+                                          <span data-icon="&#xe0ab;"></span>
+                                        </a>
+                                      )}
+                                      {attendee.info.linkedin && (
+                                        <a
+                                          target="_blank"
+                                          href={`${attendee.info.linkedin_protocol}${attendee.info.linkedin}`}
+                                        >
+                                          <span data-icon="&#xe0b1;"></span>
+                                        </a>
+                                      )}
+                                      {attendee.info.website && (
+                                        <a
+                                          target="_blank"
+                                          href={`${attendee.info.website_protocol}${attendee.info.website}`}
+                                        >
+                                          <span data-icon="&#xe0b7;"></span>
+                                        </a>
+                                      )}
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          )}
+                      </div>
+                      {/* Description */}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          {/* Grid */}
+        </div>
+        {attendees.length === 0 && <div>No Attendees Found...</div>}
+        {attendees.length > 0 && loadMore()}
+      </div>
+    </div>
+  );
+};
+
+export default Variation5;
