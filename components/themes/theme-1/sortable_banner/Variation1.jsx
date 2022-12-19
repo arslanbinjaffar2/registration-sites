f2ab0d4f3277eacd31f7c400e0e3789b37bee789
DESKTOP-9EMPNRP\Eventbuizz <hab@eventbuizz.com>
2022-12-19 19:27:06 +0500

    dev

--- components/themes/theme-1/sortable_banner/Variation1.jsx
+++ components/themes/theme-1/sortable_banner/Variation1.jsx
@@ -27,9 +27,8 @@ const Variation1 = ({ event, banner, countdown, regisrationUrl }) => {
 	}
 
 	return (
-		<div data-fixed="true" className="main-slider-wrapper ebs-transparent-box">
+		<div className="main-slider-wrapper ebs-transparent-box ebs-sortable-banner">
 			{banner && <SliderBanner
-				countdown={countdown} //{dateTime}
 				fullscreen
 			>
 				{banner.map((slides, i) =>
@@ -43,21 +42,6 @@ const Variation1 = ({ event, banner, countdown, regisrationUrl }) => {
 								</div>}
 							<div className="caption-wrapp">
 								<div className="col-12 align-items-center d-flex inner-caption-wrapp">
-									<div style={{ position: 'relative' }} className="parallax-text">
-										{slides.info.title &&
-											<div className="edgtf-custom-font-holder ebs-banner-title" style={{ fontFamily: 'Rubik', fontSize: '100px', lineHeight: '110px', fontWeight: '500', textTransform: 'uppercase', textAlign: 'left', color: '#ec008c' }}>
-												<span style={{ color: '#fff' }}> {slides.info.title} </span>
-											</div>
-										}
-										{slides.info.message && <div className="edgtf-custom-font-holder ebs-banner-subtitle"
-											style={{ marginTop: '15px', fontSize: '26px', lineHeight: '37px', fontWeight: '400', letterSpacing: '0px', textAlign: 'left', color: '#ffffff', maxWidth: 850 }}>
-											{slides.info.message}
-										</div>}
-										<div className="edgtf-custom-font-holder"
-											style={{ marginTop: '40px', fontSize: '26px', lineHeight: '37px', fontWeight: '400', letterSpacing: '0px', textAlign: 'left', color: '#ffffff' }}>
-											<a href={regisrationUrl} style={{ fontFamily: 'Rubik', marginRight: '0', fontSize: '15px', fontWeight: '500', background: 'transparent', border: '2px solid #fff', color: '#fff', padding: '17px 48px 15px' }} className="edgtf-btn edgtf-btn-huge edgtf-btn-custom-border-hover edgtf-btn-custom-hover-bg edgtf-btn-custom-hover-color">REGISTER</a>
-										</div>
-									</div>
 								</div>
 							</div>
 						</WrapperLayout>
