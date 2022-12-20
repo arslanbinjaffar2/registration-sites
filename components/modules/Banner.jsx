3fa69ab0f38830792c4cc495a2fe5f064043c60a
ahmad13054 <mah@eventbuizz.com>
2022-12-20 18:44:48 +0500

    dev

--- components/modules/Banner.jsx
+++ components/modules/Banner.jsx
@@ -18,7 +18,7 @@ const loadModule = (theme, variation) => {
 
 const Banner = () => {
   const { event } = useSelector(eventSelector);
-  const { banner } = useSelector(globalSelector);
+  const { banner, settings } = useSelector(globalSelector);
   const dispatch = useDispatch();
 
   const eventUrl = event.url;
@@ -51,7 +51,7 @@ const Banner = () => {
   }, [dispatch]);
   return (
     <Suspense fallback={<div></div>}>
-      {banner && banner?.length > 0 ? <Component regisrationUrl={regisrationUrl} banner={banner} event={event} countdown={event.eventsiteSettings.registration_end_date !== "0000-00-00 00:00:00" ? moment(event.eventsiteSettings.registration_end_date): null} /> : null}
+      {banner && banner?.length > 0 ? <Component regisrationUrl={regisrationUrl} settings={settings} banner={banner} event={event} countdown={event.eventsiteSettings.registration_end_date !== "0000-00-00 00:00:00" ? moment(event.eventsiteSettings.registration_end_date): null} /> : null}
     </Suspense>
   );
 };
