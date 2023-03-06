3b6b8aae7b4e7aeb8b2fbd0c8481a4dcceedec70
ahmad13054 <mah@eventbuizz.com>
2023-03-06 12:06:26 +0500

    dev

--- components/myAccount/profile/CancelRegistration.jsx
+++ components/myAccount/profile/CancelRegistration.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import ActiveLink from "components/atoms/ActiveLink";
 import { fetchProfileData, profileSelector, fetchInvoiceData, cancelRegistrationRequest } from 'store/Slices/myAccount/profileSlice';
 import { userSelector } from 'store/Slices/myAccount/userSlice';
@@ -16,6 +16,20 @@ const CancelRegistration = () => {
   const dispatch = useDispatch();
   const router = useRouter();
 
+  const cancellationDatePassed = useMemo(()=>{
+    if(event.eventsiteSettings.cancellation_date === "0000-00-00 00:00:00"){
+      return 0;
+    }
+    let dateToday = moment();
+    let cancelationEndDate = moment(`${moment(event.eventsiteSettings.cancellation_date).format("YYYY-MM-DD")} ${event.eventsiteSettings.cancellation_end_time}`);
+    let passed = cancelationEndDate.diff(dateToday);
+    return passed > 0 ? 0 : 1;
+  },[event]);
+
+  if(cancellationDatePassed !== 0){
+    router.push(`/${event.url}`);
+  }
+
   const cancel = async () => {
    dispatch(cancelRegistrationRequest(event.id, event.url)) 
   }
@@ -25,24 +39,26 @@ const CancelRegistration = () => {
    }
 
   return (
-    <div className="edgtf-container ebs-my-profile-area pb-5">
-      <div className="edgtf-container-inner container">
-        <div className="ebs-header text-center">
-          <h2>Are you sure you want to cancel</h2>
-        </div>
-          <div className="generic-form">
-            <p>Your comment:</p>
-            <textarea
-              placeholder="Your comment"
-              cols={30}
-              rows={5}
-              
-            ></textarea>
+    <React.Fragment>
+     {(cancellationDatePassed !== undefined && cancellationDatePassed === 0) ? <div className="edgtf-container ebs-my-profile-area pb-5">
+        <div className="edgtf-container-inner container">
+          <div className="ebs-header text-center">
+            <h2>Are you sure you want to cancel</h2>
           </div>
-        <button className="btn btn-save-next btn-loader btn-danger" onClick={()=>{ cancel() }} > Confirm cancelling registration </button>
+            <div className="generic-form">
+              <p>Your comment:</p>
+              <textarea
+                placeholder="Your comment"
+                cols={30}
+                rows={5}
+                
+              ></textarea>
+            </div>
+          <button className="btn btn-save-next btn-loader btn-danger" onClick={()=>{ cancel() }} > Confirm cancelling registration </button>
 
-      </div>
-    </div>
+        </div>
+      </div> : <PageLoader/>}
+    </React.Fragment>
   )
 }
 
