79298a32efbe5ba61bf7bc01eb05beb61ca153a7
ahmad13054 <mah@eventbuizz.com>
2023-05-03 15:31:54 +0500

    EB-18357

--- components/themes/theme-1/header/Variation1.jsx
+++ components/themes/theme-1/header/Variation1.jsx
@@ -253,15 +253,7 @@ class Variation1 extends React.Component {
                             </ActiveLink>
                           )
                         ) : (
-                          menu.alias === "info_pages" ? (
-                            <ActiveLink
-                              className="nav-link"
-                              aria-current="page"
-                              href={`/${this.props.event.url}/info_pages/${menu.page_id}`}
-                            >
-                                {menu.module}
-                            </ActiveLink>
-                          ): (
+                           (
                             <ActiveLink
                               className="nav-link" activeClassName="nav-link active"
                               aria-current="page"
@@ -577,7 +569,7 @@ class Variation1 extends React.Component {
                             )}
                           </ul>
                         )}
-                        {(menu.alias === "info_pages" && (menus["info_pages_menu"].length > 1 ||  (menus["info_pages_menu"].length == 1 && event.header_data["info_pages_menu"][0].page_type === "menu"))) && (
+                        {(menu.alias === "info_pages" && (  menus["info_pages_menu"].find((p)=>p.id == menu.page_id)['submenu'].length > 1 ||  (menus["info_pages_menu"].find((p)=>p.id == menu.page_id)['submenu'].length == 1 && menus["info_pages_menu"].find((p)=>p.id == menu.page_id)['submenu'][0].page_type === "menu"))) && (
                           <ul className="dropdown-menu">
                             {menus["info_pages_menu"].find((item)=>(item.id == menu.page_id)) !== undefined && menus["info_pages_menu"].find((item)=>(item.id == menu.page_id)).submenu.map((gItem, k) =>
                               (gItem.page_type && gItem.page_type === 1  &&  gItem.submenu && gItem.submenu.length > 0) ? (
