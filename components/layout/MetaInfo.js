0a4b17e0eef753cf99c5224081d9279511f5dacf
moazzam_eventbuizz <mms@eventbuizz.com>
2023-06-13 19:06:45 +0500

    live change

--- components/layout/MetaInfo.js
+++ components/layout/MetaInfo.js
@@ -158,32 +158,27 @@ const MetaInfo = (props) => {
                         </noscript>
                     </>
                 )}
-                
+
                 {props.metaInfo.settings?.facebook_pixel_id !== undefined && props.metaInfo.settings?.facebook_pixel_id && props.cookie !== null && props.cookie == "all" && (
-                    <>
-                        <Script
-                            id="facebook-analytics-1"
-                            src={`https://www.googletagmanager.com/gtag/js?id=${props.metaInfo.settings?.google_analytics_id}`}
-                            strategy="afterInteractive"
+                    <React.Fragment>
+                        <script dangerouslySetInnerHTML={{
+                            __html: `!function(f,b,e,v,n,t,s)
+                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                            n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];
+                            s.parentNode.insertBefore(t,s)}(window, document,'script',
+                            'https://connect.facebook.net/en_US/fbevents.js');
+                            fbq('init', '${props.metaInfo.settings?.facebook_pixel_id}');
+                            fbq('track', '${(props.metaInfo.settings?.analytics_page_view_event_name !== undefined && props.metaInfo.settings?.analytics_page_view_event_name ? props.metaInfo.settings?.analytics_page_view_event_name : 'PageView')}');`
+                        }}
                         />
-                        <Script id="facebook-analytics-2" strategy="afterInteractive">
-                            {`
-                        !function(f,b,e,v,n,t,s)
-                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                        n.queue=[];t=b.createElement(e);t.async=!0;
-                        t.src=v;s=b.getElementsByTagName(e)[0];
-                        s.parentNode.insertBefore(t,s)}(window, document,'script',
-                        'https://connect.facebook.net/en_US/fbevents.js');
-                        fbq('init', '${props.metaInfo.settings?.facebook_pixel_id}');
-                        fbq('track', '${(props.metaInfo.settings?.analytics_page_view_event_name !== undefined && props.metaInfo.settings?.analytics_page_view_event_name ? props.metaInfo.settings?.analytics_page_view_event_name : 'PageView')}');
-                        `}
-                        </Script>
-                        <noscript>
-                            <img height="1" width="1" style={{ display: 'none' }} src={`https://www.facebook.com/tr?id=${props.metaInfo.settings?.facebook_pixel_id}&ev=PageView&noscript=1`} />
-                        </noscript>
-                    </>
+                        <noscript dangerouslySetInnerHTML={{
+                            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${props.metaInfo.settings?.facebook_pixel_id}&ev=${(props.metaInfo.settings?.analytics_page_view_event_name !== undefined && props.metaInfo.settings?.analytics_page_view_event_name ? props.metaInfo.settings?.analytics_page_view_event_name : 'PageView')}&noscript=1" />`
+                        }}
+                        />
+                    </React.Fragment>
                 )}
             </Head>
         </>
