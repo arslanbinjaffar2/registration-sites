142118c62eab8543af63bff96e726945438cb687
ahmad13054 <mah@eventbuizz.com>
2023-07-11 17:09:01 +0500

    RS20-880

--- components/modules/Documents.jsx
+++ components/modules/Documents.jsx
@@ -33,7 +33,7 @@ const Documents = () => {
         <Head>
         <title>{event.eventsiteModules.documents}</title>
         </Head>
-       {documents ? <CustomComponent documents={documents} documentPage={true} labels={event.labels} /> : <PageLoader/>}
+       {documents ? <CustomComponent documents={documents} documentPage={true} labels={event.labels} eventTimezone={event.timezone.timezone} /> : <PageLoader/>}
     </Suspense>
   );
 };
