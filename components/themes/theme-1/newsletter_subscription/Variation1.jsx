dfba7cb5f97da82e2b92fdedebac7628bd37c4a8
ahmad13054 <mah@eventbuizz.com>
2023-07-26 16:10:39 +0500

    RS20-809

--- components/themes/theme-1/newsletter_subscription/Variation1.jsx
+++ components/themes/theme-1/newsletter_subscription/Variation1.jsx
@@ -9,12 +9,12 @@ const Variation1 = (props) =>  {
     props.handleSubmit({email,first_name,last_name,is_checked});
 
 }
-  const bgStyle = (props.moduleVariation && props.moduleVariation.background_color !== "") ? { backgroundColor: props.moduleVariation.background_color} : {}
+  const bgStyle = (props.moduleVariation && props.moduleVariation.background_color !== "") ? { backgroundColor: props.moduleVariation.background_color} : {backgroundColor: '#F2F2F2' }
 
     return (
       <React.Fragment>
-        {props.settings && <div style={bgStyle} className="module-section">
-          <div  style={{backgroundColor: '#F2F2F2' }} className="ebs-default-padding">
+        {props.settings && <div  className="module-section">
+          <div style={bgStyle}  className="ebs-default-padding">
           <div className="container">
             <HeadingElement dark={false} label={"Subscribe to our newsletter "}  align={'center'} />
             <p style={{color:"#fff", textAlign:"center"}} dangerouslySetInnerHTML={{__html: props.settings.content}} />
