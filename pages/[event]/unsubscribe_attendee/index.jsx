2f8eb0f7e98645bfe8a01bb22359ccd714b422b3
moazzam_eventbuizz <mms@eventbuizz.com>
2023-08-17 19:24:37 +0500

    live change

--- pages/[event]/unsubscribe_attendee/index.jsx
+++ pages/[event]/unsubscribe_attendee/index.jsx
@@ -37,7 +37,12 @@ const Index = (props) => {
             `${process.env.NEXT_APP_URL}/event/${event.url}/unsubscribe-attendee?id=${id}&event_id=${event_id}&email=${email}`
         );
         if (response.data.success) {
-            router.push(`/${event.url}`);
+            if (event?.eventsiteSettings?.third_party_redirect_url && Number(event?.eventsiteSettings?.third_party_redirect) === 1) {
+                window.location = event?.eventsiteSettings?.third_party_redirect_url;
+            }
+            else {
+                router.push(`/${event.url}`);
+            }
         } else {
             setError(response.data.message)
         }
