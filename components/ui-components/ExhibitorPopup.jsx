1d1839e99b152d007c248511305c0619116bc5b0
Aizaz khan <akh@eventbuizz.com>
2023-09-08 17:16:50 +0500

    RS20-950

--- components/ui-components/ExhibitorPopup.jsx
+++ components/ui-components/ExhibitorPopup.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ActiveLink from "components/atoms/ActiveLink";
 import Image from 'next/image'
 
-const ExhibitorPopup = ({ width, onClick, data, eventUrl }) => {
+const ExhibitorPopup = ({ width, onClick, data, eventUrl, labels }) => {
 
     React.useEffect(() => {
         if (typeof window !== 'undefined') {
@@ -42,11 +42,12 @@ const ExhibitorPopup = ({ width, onClick, data, eventUrl }) => {
                                 {data.name && <h2>{data.name}</h2>}
                                 {data.description && <p dangerouslySetInnerHTML={{ __html: data.description }}></p>}
                                 <div className="ebs-social-icons">
-                                    {data.facebook && <a href={data.facebook}><i className="fa fa-facebook" /></a>}
-                                    {data.twitter && <a href={data.twitter}><i className="fa fa-twitter" /></a>}
-                                    {data.linkedin && <a href={data.linkedin}><i className="fa fa-linkedin" /></a>}
+                                    {data.website.replace(/^https?:\/\//, "") && <a href={data.website}><span style={{ fontSize: "30px", marginLeft: "-2px" }} data-icon="&#xe0e3;"></span></a>}
+                                    {data.facebook.replace(/^https?:\/\//, "") && <a href={data.facebook}><i className="fa fa-facebook" /></a>}
+                                    {data.twitter.replace(/^https?:\/\//, "") && <a href={data.twitter}><i className="fa fa-twitter" /></a>}
+                                    {data.linkedin.replace(/^https?:\/\//, "") && <a href={data.linkedin}><i className="fa fa-linkedin" /></a>}
                                 </div>
-                                <p><ActiveLink href={`/${eventUrl}/exhibitors/${data.id}`}>Read More</ActiveLink></p>
+                                <p><ActiveLink href={data.url.replace(/^https?:\/\//, "") != "" ? data.url : `/${eventUrl}/exhibitors/${data.id}`}>{labels.EVENTSITE_READMORE ? labels.EVENTSITE_READMORE : 'Read more'}</ActiveLink></p>
                             </div>
                         </div>
                     </div>
