946ff6301df950fb3536d261331db26a8295353f
hashmat_eventbuizz <hab@eventbuizz.com>
2023-11-01 18:32:48 +0500

    master

--- components/themes/theme-1/banner/Variation5.jsx
+++ components/themes/theme-1/banner/Variation5.jsx
@@ -58,59 +58,61 @@ const Variation5 = ({ banner, event, countdown, regisrationUrl, settings, regist
                 )}
                 <div className="caption-wrapp">
                   <div className="col-12 align-items-center d-flex inner-caption-wrapp ebs-fluid-banner">
-                    <div
-                      className="parallax-text"
-                    >
-                      {slides.info.title && settings.title === 1 && (
-                        <div
-                          className="edgtf-custom-font-holder ebs-banner-title"
+                    <div className="ebs-parallax-text-container w-100 h-100 position-absolute left-0 top-0 ">
+                      <div
+                        className="parallax-text"
+                      >
+                        {slides.info.title && settings.title === 1 && (
+                          <div
+                            className="edgtf-custom-font-holder ebs-banner-title"
+                            style={{
+                              fontFamily: "Rubik",
+                              fontSize: "100px",
+                              lineHeight: "115px",
+                              fontWeight: "500",
+                              textTransform: "uppercase",
+                              textAlign: "left",
+                              color: "#ec008c",
+                            }}
+                          >
+                            <span style={{ color:  slides?.title_color ? slides?.title_color : "#fff" }}>
+                              {" "}
+                              {slides.info.title}{" "}
+                            </span>
+                          </div>
+                        )}
+                        {slides.info.message && settings.caption === 1 && (
+                          <div
+                            className="edgtf-custom-font-holder ebs-banner-subtitle"
+                            style={{
+                              marginTop: "15px",
+                              fontSize: "26px",
+                              lineHeight: "37px",
+                              fontWeight: "400",
+                              letterSpacing: "0px",
+                              textAlign: "left",
+                              color:  slides?.sub_title_color ? slides?.sub_title_color : "#fff",
+                              maxWidth: 850,
+                            }}
+                          >
+                            {slides.info.message}
+                          </div>
+                        )}
+                        {settings.register_button === 1 && registerDateEnd  && <div
+                          className="edgtf-custom-font-holder ebs-custom-button-holder"
                           style={{
-                            fontFamily: "Rubik",
-                            fontSize: "100px",
-                            lineHeight: "115px",
-                            fontWeight: "500",
-                            textTransform: "uppercase",
-                            textAlign: "left",
-                            color: "#ec008c",
-                          }}
-                        >
-                          <span style={{ color:  slides?.title_color ? slides?.title_color : "#fff" }}>
-                            {" "}
-                            {slides.info.title}{" "}
-                          </span>
-                        </div>
-                      )}
-                      {slides.info.message && settings.caption === 1 && (
-                        <div
-                          className="edgtf-custom-font-holder ebs-banner-subtitle"
-                          style={{
-                            marginTop: "15px",
+                            marginTop: "40px",
                             fontSize: "26px",
                             lineHeight: "37px",
                             fontWeight: "400",
                             letterSpacing: "0px",
                             textAlign: "left",
-                            color:  slides?.sub_title_color ? slides?.sub_title_color : "#fff",
-                            maxWidth: 850,
+                            color: "#ffffff",
                           }}
                         >
-                          {slides.info.message}
-                        </div>
-                      )}
-                      {settings.register_button === 1 && registerDateEnd  && <div
-                        className="edgtf-custom-font-holder ebs-custom-button-holder"
-                        style={{
-                          marginTop: "40px",
-                          fontSize: "26px",
-                          lineHeight: "37px",
-                          fontWeight: "400",
-                          letterSpacing: "0px",
-                          textAlign: "left",
-                          color: "#ffffff",
-                        }}
-                      >
-                        <a href={regisrationUrl} style={{ fontFamily: 'Rubik', marginRight: '0', fontSize: '15px', fontWeight: '500', background: 'transparent', border: '2px solid #fff', color: '#fff', padding: '17px 48px 15px' }} className="edgtf-btn edgtf-btn-huge edgtf-btn-custom-border-hover edgtf-btn-custom-hover-bg edgtf-btn-custom-hover-color">{event.labels.EVENTSITE_REGISTER_NOW2 ? event.labels.EVENTSITE_REGISTER_NOW2 : 'Register Now'}</a>
-                      </div>}
+                          <a href={regisrationUrl} style={{ fontFamily: 'Rubik', marginRight: '0', fontSize: '15px', fontWeight: '500', background: 'transparent', border: '2px solid #fff', color: '#fff', padding: '17px 48px 15px' }} className="edgtf-btn edgtf-btn-huge edgtf-btn-custom-border-hover edgtf-btn-custom-hover-bg edgtf-btn-custom-hover-color">{event.labels.EVENTSITE_REGISTER_NOW2 ? event.labels.EVENTSITE_REGISTER_NOW2 : 'Register Now'}</a>
+                        </div>}
+                      </div>
                     </div>
                   </div>
                 </div>
