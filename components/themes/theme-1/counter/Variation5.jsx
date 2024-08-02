96dbe9a9fda42732b1080086671dc33d40078e8b
EVENTBUIZZ\hamza <hla@eventbuizz.com>
2024-08-02 15:30:05 +0500

    EB-21624

--- components/themes/theme-1/counter/Variation5.jsx
+++ components/themes/theme-1/counter/Variation5.jsx
@@ -5,11 +5,11 @@ import Countdown, {zeroPad} from "react-countdown";
 
 const Completionist = ({ event, completed }) => (
   <div className="col-12">
-    <div className="text-center fs-4 text-danger pt-5">
-      {completed && event.count_down_section && event.count_down_section.expiry_message && (
-        <div dangerouslySetInnerHTML={{ __html: event.count_down_section.expiry_message }} />
+    {completed && event.count_down_section && event.count_down_section.expiry_message && (
+      <div className="text-center fs-4 text-danger pt-5">
+          <div dangerouslySetInnerHTML={{ __html: event.count_down_section.expiry_message }} />
+      </div>
       )}
-    </div>
   </div>
 );
 const Variation5 = ({ event, labels, settings }) => {
