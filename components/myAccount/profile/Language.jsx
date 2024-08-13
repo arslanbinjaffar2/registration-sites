8f816959d52500df322c2985002ca053bf3fcaee
hashmat_eventbuizz <hab@eventbuizz.com>
2024-08-13 11:49:38 +0500

    APP-1135

--- components/myAccount/profile/Language.jsx
+++ components/myAccount/profile/Language.jsx
@@ -35,18 +35,16 @@ const Language = () => {
   return (
     <React.Fragment>
       <div className="ebs-profile-top-area">
-        <div onClick={handleClick} className="ebs-sideber-icon">
-          <div className="d-flex align-items-center">
-            <span className="d-block position-relative" style={{ width: 26, height: 26 }}>
-              {settings?.profile_picture?.status === 1 && attendee?.image ? (
-                <img className="ebs-image-solid" width="26" src={
-                  `${process.env.NEXT_APP_EVENTCENTER_URL}/assets/attendees/${attendee?.image}`
-                } alt="" />
-              ) : (
-                <Image objectFit="contain" width="26" layout="fill" className="ebs-image-solid" src={
-                  require("public/img/square.jpg")
-                } alt="" />
-              )}
+        <div onClick={handleClick} className="ebs-sideber-icond">
+          <div style={{cursor: 'pointer'}} className="d-flex align-items-center border rounded-1 py-1 px-2">
+            <span className="d-block position-relative d-flex align-items-center">
+               <img 
+                    className="ebs-image-solid" 
+                    width="16" 
+                    src={`${process.env.NEXT_APP_EVENTCENTER_URL}/_eventsite_assets/language_switcher/${languages[0]?.name.toLowerCase()}.jpg`} 
+                    alt={languages?.name} 
+                />
+                <span className="ms-2" style={{fontSize: 12}}>{languages[0].name}</span>
             </span>
             <i style={{ fontSize: 16 }} className="material-icons ml-2">
               {toggleMenu ? "expand_less" : "expand_more"}
@@ -55,39 +53,39 @@ const Language = () => {
         </div>
 
         {toggleMenu && (
-          <ul className={`dropdown-menu ${toggleMenu ? "show" : ""}`} aria-labelledby="languageDropdown">
+          <ul style={{minWidth: 180}} className={`dropdown-menu mt-2 end-0 start-auto  ${toggleMenu ? "show" : ""}`} aria-labelledby="languageDropdown">
             {languages.map((language, index) => (
               <li key={language.id}>
                 {console.log(eventDetails[index][0],'innn')}
                 <a 
                 
-                  className="dropdown-item p-2 ps-2" 
+                  className="dropdown-item py-1 lh-base px-3" 
                   onClick={() => handleLanguageClick(eventDetails[index]?.url)}
                 >
                    <img 
-                  key={index} 
-                  className="ebs-image-solid" 
-                  width="26" 
-                  src={`${process.env.NEXT_APP_EVENTCENTER_URL}/_eventsite_assets/language_switcher/${language?.name.toLowerCase()}.jpg`} 
-                  alt={language?.name} 
+                    key={index} 
+                    className="ebs-image-solid" 
+                    width="21" 
+                    src={`${process.env.NEXT_APP_EVENTCENTER_URL}/_eventsite_assets/language_switcher/${language?.name.toLowerCase()}.jpg`} 
+                    alt={language?.name} 
                 />
-                  {language.name}
+                  <span className="ms-2" style={{fontSize: 12}}>{language?.name}</span>
                 </a>
               </li>
             ))}
             <li >
               
                 <a 
-                  className="dropdown-item p-2" 
+                  className="dropdown-item py-1 lh-base px-3"  
                   onClick={() => handleLanguageClick(from_event_detail?.[0]?.url)}
                 >
                    <img 
                   className="ebs-image-solid" 
-                  width="26" 
+                  width="21" 
                   src={`${process.env.NEXT_APP_EVENTCENTER_URL}/_eventsite_assets/language_switcher/${languages[0]?.name.toLowerCase()}.jpg`} 
                   alt={languages[0]?.name} 
                 />
-                  {event.language.name}
+                  <span className="ms-2" style={{fontSize: 12}}>{event.language.name}</span>
                 </a>
               </li>
           </ul>
