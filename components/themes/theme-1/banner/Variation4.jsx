e2ddf24313bb7cf8042bf640efbab77c3b91bac6
EVENTBUIZZ\hamza <hla@eventbuizz.com>
2024-10-10 15:08:39 +0500

    RS20-1573

--- components/themes/theme-1/banner/Variation4.jsx
+++ components/themes/theme-1/banner/Variation4.jsx
@@ -102,7 +102,7 @@ const Variation4 = ({ banner, event, countdown, regisrationUrl, settings, regist
                             {slides.info.message}
                           </div>
                         )}
-                        {settings.register_button && registerDateEnd && <div
+                        {settings?.register_button && registerDateEnd  && <div
                           className="edgtf-custom-font-holder text-center ebs-custom-button-holder"
                           style={{
                             marginTop: "40px",
@@ -125,7 +125,7 @@ const Variation4 = ({ banner, event, countdown, regisrationUrl, settings, regist
           </SliderBanner>
         )}
       </div>
-      {settings.register_button === 1 && registerDateEnd  && <div  className='ebs-mobile-register-button py-4 d-flex align-items-center justify-content-center'>
+      {settings?.register_button === 1 && registerDateEnd  && <div  className='ebs-mobile-register-button py-4 d-flex align-items-center justify-content-center'>
 			<div className="edgtf-custom-font-holder ebs-custom-button-holder"
 				style={{ marginTop: '0', fontSize: '26px', lineHeight: '37px', fontWeight: '400', letterSpacing: '0px', textAlign: 'left', color: '#444' }}>
 				<a href={regisrationUrl} style={{ fontFamily: 'Rubik', marginRight: '0', fontSize: '15px', fontWeight: '500', background: 'transparent', border: '2px solid #444', color: '#444', padding: '17px 48px 15px' }} className="edgtf-btn edgtf-btn-huge edgtf-btn-custom-border-hover edgtf-btn-custom-hover-bg edgtf-btn-custom-hover-color">{event.labels.EVENTSITE_REGISTER_NOW2 ? event.labels.EVENTSITE_REGISTER_NOW2 : 'Register Now'}</a>
